74cc85c38abb64a58a353a4c1bcadf17c285fc7e
jesings <jsinger@stuy.edu>
2018-12-13 09:51:37 -0500

    fixed it

--- tele.c
+++ tele.c
@@ -5,8 +5,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/shm.h>
-#include <fcntl.h>
-#include <unistd.h>
+#include <errno.h>
 
 union semun {
                int              val;    /* Value for SETVAL */
@@ -15,6 +14,7 @@ union semun {
                struct seminfo  *__buf;  /* Buffer for IPC_INFO
                                            (Linux-specific) */
            };
+
 
 
 int main(int argc,char** argv){
@@ -31,26 +31,24 @@ int main(int argc,char** argv){
         printf("Give a valid command line argument dummy\n");
         return -1;
     }
-    key_t mykey = ftok("phoneline",'R');
+    key_t mykey = ftok("tele.c",'R');
     int shmid,semid,fd;
     if(mykey){
         switch(mode){
             case 'c':
                 shmid = shmget(mykey,2048,0644|IPC_CREAT|IPC_EXCL);
                 semid = semget(mykey,1,0644|IPC_CREAT|IPC_EXCL);
-                //fd = open("teledata.txt",O_RDWR|O_CREAT|O_TRUNC,0644);
-                if(!((shmid==-1)||(semid==-1)/*|fd*/)){
-                    printf("Shmcreation failure\n");
+                if((shmid==-1)||(semid==-1)){
+                    printf("Shm/semcreation failure\n");
                     return -1;
                 }
-                //s.val = 1;
                 semctl(semid,0,SETVAL,1);
                 break;
             case 'r':
-                shmid = shmget(mykey,2048,0644);
+                shmid = shmget(mykey,2048,0);
                 semid = semget(mykey,1,0);
-                if((shmid!=-1)&&(semid!=-1)){
-                    printf("Shmget failure\n");
+                if((shmid==-1)||(semid==-1)){
+                    printf("Shmget/Semget failure\n");
                     return -1;
                 }
                 struct sembuf sb;
@@ -58,10 +56,13 @@ int main(int argc,char** argv){
                 semop(semid,&sb,1);
                 shmctl(shmid, IPC_RMID, NULL);
                 semctl(semid, IPC_RMID, 0);
-                //remove("teledata.txt");
                 break;
             case 'v':
                 shmid = shmget(mykey,2048,0);
+                if(shmid==-1){
+                    printf("Shmget failure\n");
+                    return -1;
+                }
                 char* str = shmat(shmid,(void*)0,0); 
                 printf("This is the story thusfar:\n");
                 puts(str);
